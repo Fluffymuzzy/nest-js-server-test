4fcd3af024c01b106a0e7fced464b49318688855
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-05-27 20:31:06 +0300

    finished with crud

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,4 +1,13 @@
-import { Post, Body } from "@nestjs/common";
+import {
+  Post,
+  Body,
+  Put,
+  Param,
+  HttpCode,
+  HttpStatus,
+  Delete,
+  Get,
+} from "@nestjs/common";
 import { Controller } from "@nestjs/common";
 import { ProductsService } from "./products.service";
 import { Product } from "./product.model";
@@ -6,9 +15,35 @@ import { Product } from "./product.model";
 @Controller("admin/products")
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
-
+  // ----------------------
   @Post()
   async createProduct(@Body() productData: Partial<Product>): Promise<Product> {
     return this.productsService.createProduct(productData);
+  }
+  // ----------------------
+  @Put(":id")
+  @HttpCode(HttpStatus.OK)
+  async updateProduct(
+    @Param("id") id: number,
+    @Body() productData: Partial<Product>
+  ): Promise<Product> {
+    return this.productsService.updateProduct(id, productData);
+  }
+  // ----------------------
+  @Delete(":id")
+  async deleteProduct(@Param("id") id: number): Promise<string> {
+    return this.productsService.deleteProduct(id);
+  }
+  // ----------------------
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getAllProducts(): Promise<Product[]> {
+    return this.productsService.getAllProducts();
+  }
+  // ----------------------
+  @Get(":id")
+  @HttpCode(HttpStatus.OK)
+  async getProductById(@Param("id") id: number): Promise<Product | string> {
+    return this.productsService.getProductById(id);
   }
 }
--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
 import { Product } from "./product.model";
 
@@ -6,12 +6,48 @@ import { Product } from "./product.model";
 export class ProductsService {
   constructor(
     @InjectModel(Product)
-    private productModel: typeof Product,
+    private productModel: typeof Product
   ) {}
-
+  // ----------------------
   async createProduct(productData: Partial<Product>): Promise<Product> {
     return this.productModel.create(productData);
   }
+  // ----------------------
+  async updateProduct(
+    id: number,
+    productData: Partial<Product>
+  ): Promise<Product> {
+    await this.productModel.update(productData, {
+      where: { id },
+    });
+    const updatedProduct = await this.productModel.findByPk(id);
+    if (!updatedProduct) {
+      throw new NotFoundException("Product not found");
+    }
+    return updatedProduct;
+  }
+  // ----------------------
+  async deleteProduct(id: number): Promise<string> {
+    const res = await this.productModel.destroy({
+      where: { id },
+    });
 
-
+    if (res === 1) {
+      return "the product was successfully deleted";
+    } else if (res === 0) {
+      return "the product wasn't successfully deleted, some f***ng problems";
+    }
+  }
+  // ----------------------
+  async getAllProducts(): Promise<Product[]> {
+    return this.productModel.findAll();
+  }
+  // ----------------------
+  async getProductById(id: number): Promise<Product | string> {
+    const product = await this.productModel.findByPk(id);
+    if (!product) {
+      return "the product was not found";
+    }
+    return product;
+  }
 }
